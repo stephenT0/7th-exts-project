e8676dd15b7e4c4ef1582ae7d9acf40642ebe8ce
Stephen Tong/Funky Boy <stephen.tong@mail.utoronto.ca>
2022-09-04 13:12:40 -0400

    initial commit

---
+++ exts/Funkyboy.cameraspeed/Funkyboy/cameraspeed/extension.py
@@ -0,0 +1,35 @@
+#__all__ = ["ExampleWindowExtension"]
+
+from .window import SimpleCamWindow, WINDOW_TITLE
+import omni.ext
+import omni.kit.ui
+
+
+class SimpleCamExtension(omni.ext.IExt):
+
+
+    def on_startup(self, ext_id):
+        self._menu_path = f"Window/{WINDOW_TITLE}"
+        self._window = SimpleCamWindow(WINDOW_TITLE, self._menu_path)
+        self._menu = omni.kit.ui.get_editor_menu().add_item(self._menu_path, self._on_menu_click, True)
+
+
+    def on_shutdown(self):
+        omni.kit.ui.get_editor_menu().remove_item(self._menu)
+        if self._window is not None:
+            self._window.destroy()
+            self._window = None
+
+
+    def _on_menu_click(self, menu, toggled):
+        if toggled:
+            if self._window is None:
+                self._window = SimpleCamWindow(WINDOW_TITLE, self._menu_path)
+            else:
+                self._window.show()
+        else:
+            if self._window is not None:
+                self._window.hide()
+    
+
+
---
+++ exts/Funkyboy.cameraspeed/docs/CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## [0.1.0] 202-08-18
+-Initial version of Simple Camera Speed Selector
